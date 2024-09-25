76ab573da240e319ef578d9e1b5ba6f74f39dedd
camachoRamel <142206766+camachoRamel@users.noreply.github.com>
2024-09-25 15:18:02 +0800

    added models and migrations

---
+++ app/Models/WeeklyTask.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class WeeklyTask extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function userTask() : HasOne
+    {
+        return $this->hasOne(UserTask::class);
+    }
+
+    public function weeklyReport() : HasOne
+    {
+        return $this->hasOne(WeeklyReport::class);
+    }
+
+    public function weeklyEvaluation() : HasOne
+    {
+        return $this->hasOne(WeeklyEvaluation::class);
+    }
+}
