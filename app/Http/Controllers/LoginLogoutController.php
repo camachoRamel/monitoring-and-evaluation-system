a872fbc5fb33a742a380804891679d15d367eebc
camachoRamel <142206766+camachoRamel@users.noreply.github.com>
2024-09-28 13:24:24 +0800

    login and logout

---
+++ app/Http/Controllers/LoginLogoutController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginLogoutController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials))
+        {
+            $request->session()->regenerate();
+
+            //switch case to determine the role of the user
+            switch(Auth::user()->role)
+            {
+                case 0:
+                    return redirect()->route('admin.index', Auth::id());
+                    break;
+
+                case 1:
+                    return redirect()->route('hte.index', Auth::id());
+                    break;
+
+                case 2:
+                    return redirect()->route('coord.index', Auth::id());
+                    break;
+
+                case 3:
+                    return redirect()->route('stud.index', Auth::id());
+                    break;
+            }
+        }
+
+        //return if login failed
+        return redirect('/')->with('incorrect', 'Incorrect password or username');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerate();
+        return redirect('/');
+    }
+}
