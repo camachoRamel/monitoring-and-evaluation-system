27d437b625ab042ce6af3c6568aecd85abb56968
Jovinil <jovinilavila123@gmail.com>
2024-10-13 01:37:31 +0800

    added evaluation modal

--- resources/views/pages/ojt_coordinator/redirection/view-student-specific-submission.blade.php
+++ resources/views/pages/ojt_coordinator/redirection/view-student-specific-submission.blade.php
@@ -9,6 +9,32 @@
 
     <section class="content w-100 px-4">
         <div class="card card-solid p-4 d-flex align-items-center">
+             <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#evaluationModal">
+            Submit Evaluation to Admin
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="evaluationModal" tabindex="-1" aria-labelledby="evaluationModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="evaluationModalLabel">Submit Evaluation</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group mb-3">
+                            <label for="file">Upload file:</label>
+                            <input type="file" name="files" id="file" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary"></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
                 <div class="card bg-light d-flex flex-fill">
                   <div class="card-header text-muted border-bottom-0">
