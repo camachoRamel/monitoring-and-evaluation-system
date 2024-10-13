dc904921885335e74df649008f6fb84056abf34d
camachoRamel <142206766+camachoRamel@users.noreply.github.com>
2024-10-13 19:26:24 +0800

    FIXED STUDENT INDEX BUG

--- resources/views/pages/student/index.blade.php
+++ resources/views/pages/student/index.blade.php
@@ -30,7 +30,7 @@
                 </thead>
                 <tbody>
 
-                    @for ($i = 0; $i < $tasks[$i]->week; $i++)
+                    @for ($i = 0; $i <= $tasks->count(); $i++)
                         {{-- display week --}}
                         <tr>
                             <td>Week {{ $tasks[$i]->week }}</td>
