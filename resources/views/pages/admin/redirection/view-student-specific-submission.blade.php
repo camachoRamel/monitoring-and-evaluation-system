8261ef7918d8d0aa2854c767a21671ac3d162a88
Jovinil <jovinilavila123@gmail.com>
2024-10-13 18:31:26 +0800

    fixed the profile output

--- resources/views/pages/admin/redirection/view-student-specific-submission.blade.php
+++ resources/views/pages/admin/redirection/view-student-specific-submission.blade.php
@@ -37,7 +37,10 @@
                         </ul>
                       </div>
                       <div class="col-5 text-center">
-                        <img src="{{ asset($stud->stud_picture == null ? 'images/profile.jpg' : 'storage/images/' . $stud->$stud_picture) }}" alt="user-avatar" class="rounded-circle img-fluid">
+                        <img
+                        src="{{ asset($stud->stud_picture == null ? 'images/profile.jpg' : 'storage/' . $stud->stud_picture) }}"
+                        alt="user-avatar"
+                        class="rounded-circle img-fluid">
                       </div>
                     </div>
                   </div>
