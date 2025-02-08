deb92180b64df705ee07eac0ef19c32ef6d288f9
Jovinil <jovinilavila123@gmail.com>
2025-02-08 23:40:00 +0800

    upload hte upload task (front-end only)

--- resources/views/pages/hte/redirection/upload-student-task.blade.php
+++ resources/views/pages/hte/redirection/upload-student-task.blade.php
@@ -75,7 +75,6 @@
                                                     @enderror
                                                 </div>
                                             </div>
-
                                             <div class="col-12 col-md-6">
                                                 <!-- Deadline input -->
                                                 <div class="form-group">
